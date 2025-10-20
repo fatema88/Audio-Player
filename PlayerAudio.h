2fc17cab665df9c569662c9221d3123af4fcdcf4
fatema88 <fatematarek88@gmail.com>
2025-10-20 23:45:49 +0300

    Update PlayerAudio.h

--- PlayerAudio.h
+++ PlayerAudio.h
@@ -1,1 +1,29 @@
 #pragma once
+
+#include <JuceHeader.h>
+
+class PlayerAudio
+{
+public:
+    PlayerAudio();
+    ~PlayerAudio();
+
+    void prepareToPlay(int samplesPerBlockExpected, double sampleRate);
+    void getNextAudioBlock(const juce::AudioSourceChannelInfo& bufferToFill);
+    void releaseResources();
+
+    bool loadFile(const juce::File& file);
+    void play();
+    void stop();
+    void setGain(float gain);
+    void setPosition(double pos);
+    double getPosition() const;
+    double getLength() const;
+
+private:
+    juce::AudioFormatManager formatManager;
+    std::unique_ptr<juce::AudioFormatReaderSource> readerSource;
+    juce::AudioTransportSource transportSource;
+};
+
+
