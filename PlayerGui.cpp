736a4489c65a31ad110ae6fae96468a918a9a8de
fatema88 <fatematarek88@gmail.com>
2025-10-24 15:43:00 +0300

    Update PlayerGui.cpp

--- PlayerGui.cpp
+++ PlayerGui.cpp
@@ -2,11 +2,12 @@
 
 PlayerGUI::PlayerGUI()
 {
-    for (auto* btn : { &loadButton, &restartButton, &stopButton,&loopButton })
+    for (auto* btn : { &loadButton, &restartButton, &stopButton, &loopButton })
     {
         btn->addListener(this);
         addAndMakeVisible(btn);
     }
+
     for (auto* btn : { &playButton, &pauseButton, &goToStartButton, &goToEndButton })
     {
         btn->addListener(this);
@@ -18,13 +19,20 @@ PlayerGUI::PlayerGUI()
     volumeSlider.addListener(this);
     addAndMakeVisible(volumeSlider);
 
-    setSize(500, 250);
+    muteButton.addListener(this);
+    addAndMakeVisible(muteButton);
 
+    loopButton.setButtonText("Loop: OFF");
+    muteButton.setButtonText("Mute");
+
+    isMuted = false;
+    isLooping = false;
+    previousVol = 0.5f;
+
+    setSize(600, 250);
 }
 
-PlayerGUI::~PlayerGUI()
-{
-}
+PlayerGUI::~PlayerGUI() {}
 
 void PlayerGUI::paint(juce::Graphics& g)
 {
@@ -35,18 +43,18 @@ void PlayerGUI::resized()
 {
     int y = 20;
     loadButton.setBounds(20, y, 100, 40);
-    playButton.setBounds(140, y, 80, 40); 
-    restartButton.setBounds(140, y, 80, 40);
-    pauseButton.setBounds(240, y, 80, 40);   
+    playButton.setBounds(140, y, 80, 40);
+    pauseButton.setBounds(240, y, 80, 40);
     stopButton.setBounds(340, y, 80, 40);
-    loopButton.setBounds(440, y, 80, 40);
+    restartButton.setBounds(440, y, 80, 40);
+    loopButton.setBounds(540, y, 100, 40);
 
-   
-    int y2 = y + 50; 
-    goToStartButton.setBounds(20, y2, 60, 40);  
-    goToEndButton.setBounds(100, y2, 60, 40);  
+    int y2 = y + 60;
+    goToStartButton.setBounds(20, y2, 100, 40);
+    goToEndButton.setBounds(140, y2, 100, 40);
 
     volumeSlider.setBounds(20, 150, getWidth() - 40, 30);
+    muteButton.setBounds(20, 200, 100, 40);
 }
 
 void PlayerGUI::buttonClicked(juce::Button* button)
@@ -56,7 +64,7 @@ void PlayerGUI::buttonClicked(juce::Button* button)
         fileChooser = std::make_unique<juce::FileChooser>(
             "Select an audio file...",
             juce::File{},
-            ".wav;*.mp3");
+            ".wav;.mp3");
 
         fileChooser->launchAsync(
             juce::FileBrowserComponent::openMode | juce::FileBrowserComponent::canSelectFiles,
@@ -73,10 +81,10 @@ void PlayerGUI::buttonClicked(juce::Button* button)
 
     if (button == &restartButton)
         playerAudio.play();
-    
-    if (button == &playButton)      
+
+    if (button == &playButton)
         playerAudio.play();
-    
+
     if (button == &pauseButton)
         pauseButtonClicked();
 
@@ -96,15 +104,18 @@ void PlayerGUI::buttonClicked(juce::Button* button)
         playerAudio.setLooping(isLooping);
     }
 
-    if (button == &muteButton) {
-        if (!isMuted) {
+    if (button == &muteButton)
+    {
+        if (!isMuted)
+        {
             previousVol = volumeSlider.getValue();
             playerAudio.setGain(0.0f);
             volumeSlider.setValue(0.0);
             muteButton.setButtonText("Unmute");
             isMuted = true;
         }
-        else {
+        else
+        {
             playerAudio.setGain(previousVol);
             volumeSlider.setValue(previousVol);
             muteButton.setButtonText("Mute");
@@ -112,8 +123,6 @@ void PlayerGUI::buttonClicked(juce::Button* button)
         }
     }
 }
-
-
 
 void PlayerGUI::sliderValueChanged(juce::Slider* slider)
 {
@@ -151,7 +160,3 @@ void PlayerGUI::goToEndButtonClicked()
 {
     playerAudio.goToEnd();
 }
-
-
-
-
