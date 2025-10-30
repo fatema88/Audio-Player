f2d8a76bcbba6d4858e14b1c2a79bf610ceccf4a
fatema88 <fatematarek88@gmail.com>
2025-10-30 23:50:49 +0200

    Update PlayerGui.h

--- PlayerGui.h
+++ PlayerGui.h
@@ -17,10 +17,19 @@ public:
     void prepareToPlay(int samplesPerBlockExpected, double sampleRate);
     void getNextAudioBlock(const juce::AudioSourceChannelInfo& bufferToFill);
     void releaseResources();
+    
 
 private:
     PlayerAudio playerAudio;
 
+    double aPoint = 0.0;
+    double bPoint = 0.0;
+
+    juce::Slider positionSlider;
+    juce::Label posLabel{
+        "Position","00:00"
+    };
+    
     juce::TextButton loadButton{ "Load File" };
     juce::TextButton restartButton{ "Restart" };
     juce::TextButton stopButton{ "Stop" };
@@ -30,8 +39,15 @@ private:
     juce::TextButton goToStartButton{ "<<" };
     juce::TextButton goToEndButton{ ">>" };
     juce::TextButton muteButton{ "Mute" };
+    juce::TextButton setAButton{ "Set A" };
+    juce::TextButton setBButton{ "Set B" };
+    juce::TextButton clearABButton{ "clear AB" };
+
+
     bool isMuted = false;
     float previousVol = 0.5f;
+
+
 
     juce::Slider volumeSlider;
     std::unique_ptr<juce::FileChooser> fileChooser;
@@ -45,4 +61,3 @@ private:
 
     JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR(PlayerGUI)
 };
-
