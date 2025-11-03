aca5146329e7f364fe53c528b3d490aae0cc4791
sarah9632 <saronamody643@gmail.com>
2025-11-03 16:35:09 +0200

    added to GUI
    Added new UI components and functionality for playlist management, reverb control, and metadata display.

--- PlayerGui.h
+++ PlayerGui.h
@@ -1,11 +1,13 @@
-
 #pragma once
 #include <JuceHeader.h>
 #include "PlayerAudio.h"
 
 class PlayerGUI : public juce::Component,
     public juce::Button::Listener,
-    public juce::Slider::Listener
+    public juce::Slider::Listener,
+    public juce::ComboBox::Listener,
+    public juce::ListBoxModel,
+    public juce::Timer
 {
 public:
     PlayerGUI();
@@ -17,7 +19,17 @@ public:
     void prepareToPlay(int samplesPerBlockExpected, double sampleRate);
     void getNextAudioBlock(const juce::AudioSourceChannelInfo& bufferToFill);
     void releaseResources();
-    
+    void updateUI();
+
+    int getNumRows() override;
+    void paintListBoxItem(int rowNumber, juce::Graphics& g,
+        int width, int height, bool rowIsSelected) override;
+    void listBoxItemClicked(int row, const juce::MouseEvent&) override;
+    void listBoxItemDoubleClicked(int row, const juce::MouseEvent&) override;
+    juce::Component* refreshComponentForRow(int rowNumber, bool isRowSelected,
+        juce::Component* existingComponentToUpdate) override;
+
+    void timerCallback() override; 
 
 private:
     PlayerAudio playerAudio;
@@ -26,10 +38,8 @@ private:
     double bPoint = 0.0;
 
     juce::Slider positionSlider;
-    juce::Label posLabel{
-        "Position","00:00"
-    };
-    
+    juce::Label posLabel{ "Position", "00:00" };
+
     juce::TextButton loadButton{ "Load File" };
     juce::TextButton restartButton{ "Restart" };
     juce::TextButton stopButton{ "Stop" };
@@ -41,23 +51,39 @@ private:
     juce::TextButton muteButton{ "Mute" };
     juce::TextButton setAButton{ "Set A" };
     juce::TextButton setBButton{ "Set B" };
-    juce::TextButton clearABButton{ "clear AB" };
+    juce::TextButton clearABButton{ "Clear AB" };
+    juce::ToggleButton reverbToggle{ "Reverb" };
+    juce::Slider reverbSlider;
+    juce::Label reverbLabel{ "Reverb Level", "Reverb:" };
 
+    juce::TextEditor metadataDisplay;
+
+    juce::ListBox playlistBox;
+    juce::TextButton nextButton{ "Next >" };
+    juce::TextButton prevButton{ "< Prev" };
+    juce::TextButton removeButton{ "Remove" };
+    juce::TextButton clearButton{ "Clear All" };
+
+    std::vector<juce::File> playlist;
+    int currentTrackIndex = -1;
 
     bool isMuted = false;
     float previousVol = 0.5f;
-
-
-
     juce::Slider volumeSlider;
     std::unique_ptr<juce::FileChooser> fileChooser;
     bool isLooping = false;
 
     void buttonClicked(juce::Button* button) override;
     void sliderValueChanged(juce::Slider* slider) override;
+    void comboBoxChanged(juce::ComboBox* comboBoxThatHasChanged) override;
     void pauseButtonClicked();
     void goToStartButtonClicked();
     void goToEndButtonClicked();
+    void displayMetadata();
+    void loadPlaylistFile(const juce::File& file);
+    void updatePlaylistDisplay();
+    void removeSelectedTrack();
+    void clearPlaylist();
 
     JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR(PlayerGUI)
 };
