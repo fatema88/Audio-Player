f39f2a5ad2ecee1441cfc73ed28d4ab667244ce8
sarah9632 <saronamody643@gmail.com>
2025-11-05 17:48:17 +0200

    Enhance MainComponent with mixing features
    Added mixing functionality with volume sliders and a toggle button.

--- MainComponent.h
+++ MainComponent.h
@@ -1,9 +1,8 @@
-
 #pragma once
 #include "JuceHeader.h"
 #include "PlayerGUI.h"
 
-class MainComponent : public juce::AudioAppComponent, public juce::Timer
+class MainComponent : public juce::AudioAppComponent
 {
 public:
     MainComponent();
@@ -13,11 +12,18 @@ public:
     void getNextAudioBlock(const juce::AudioSourceChannelInfo& bufferToFill) override;
     void releaseResources() override;
     void resized() override;
-    void timerCallback() override;
-
+    bool isMixing() const { return mixingEnabled; }
 
 private:
     PlayerGUI player1;
     PlayerGUI player2;
+
+    juce::Slider mixVol1, mixVol2;
+    juce::ToggleButton mixToggle;
+    juce::Label mixLabel;
+    bool mixingEnabled = false;
+
+    void mixTracks(const juce::AudioSourceChannelInfo& bufferToFill);
+
     JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR(MainComponent)
 };
